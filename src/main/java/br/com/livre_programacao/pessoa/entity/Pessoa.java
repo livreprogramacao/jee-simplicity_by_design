10cf500b25e61557bbd40c0d318abee19c8b94c4
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-02-02 06:14:47 -0200

    Update pojo entity
    Implemented default constructor.
    Implemented methods: equals(), hashcode() and toString().

--- src/main/java/br/com/livre_programacao/pessoa/entity/Pessoa.java
+++ src/main/java/br/com/livre_programacao/pessoa/entity/Pessoa.java
@@ -5,6 +5,7 @@
  */
 package br.com.livre_programacao.pessoa.entity;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import static javax.persistence.GenerationType.SEQUENCE;
@@ -28,7 +29,7 @@ public class Pessoa {
     private String estado;
     private String cep;
     private String telefone;
-    private String cpf;
+    private String cpf;// JPA specification
     private String rg;
     private String hobby1;
     private String hobby2;
@@ -45,4 +46,39 @@ public class Pessoa {
     private String alimento3;
     private String alimento4;
     private String alimento5;
+
+    public Pessoa() {
+        // JPA specification
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", complemento=" + complemento + ", cidade=" + cidade + ", estado=" + estado + ", cep=" + cep + ", telefone=" + telefone + ", cpf=" + cpf + ", rg=" + rg + ", hobby1=" + hobby1 + ", hobby2=" + hobby2 + ", hobby3=" + hobby3 + ", hobby4=" + hobby4 + ", hobby5=" + hobby5 + ", esporte1=" + esporte1 + ", esporte2=" + esporte2 + ", esporte3=" + esporte3 + ", esporte4=" + esporte4 + ", esporte5=" + esporte5 + ", alimento1=" + alimento1 + ", alimento2=" + alimento2 + ", alimento3=" + alimento3 + ", alimento4=" + alimento4 + ", alimento5=" + alimento5 + '}';
+    }
+
 }
